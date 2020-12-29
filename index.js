a585d88683aa26c56941b25b1abdb81dd862f292
lucasmmaidana <lucasmaidanasoy@gmail.com>
2020-12-29 19:53:54 -0300

    projects display

---
+++ index.js
@@ -0,0 +1,34 @@
+import projects from './projects.js'
+
+const projectsDiv = document.getElementById('projects')
+
+let html = ""
+
+projects.forEach(project => {
+
+    let stack = ""
+    project.stack.forEach(tech => {
+        stack += `<span>${tech}</span> `
+    })
+
+    html += `
+        <div class="project">
+            <div class="project__img">
+             <a href="${project.demo && project.demo}"><img src="${project.img}" alt="${project.name}"></a>
+            </div>
+            <div class="project__info">
+                <h3>${project.name}</h3>
+                <p class="project__description">${project.description}</p>
+                <div class="project__stack">
+                    ${stack}
+                </div>
+                <div class="project__links">
+                    <a href="${project.demo && project.demo}">Demo</a>
+                    <a href="${project.repo && project.repo}">Code</a>
+                </div>
+            </div>
+        </div>
+    `
+})
+
+projectsDiv.innerHTML = html
---
+++ projects.js
@@ -0,0 +1,13 @@
+const projects = [
+    {
+        name: "Tienda Split",
+        img: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F4783cc56-5abe-435e-b45f-b945b77d8c9b%2FsorteoMesa_de_trabajo_1_copiatribus.png?table=block&id=765a6466-e818-4955-ba95-24e197e1cfa0&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+        repo: "https://github.com/lucasmmaidana/Tienda-Split",
+        demo: "https://tienda-split.vercel.app/",
+        stack: ["HTML","SCSS","JavaScript"],
+        description: "Ecommerce integrated with WhatsApp"
+    },
+
+]
+
+export default projects
