db46d911c12607780de4102de086779ee9ca278d
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-01-15 22:04:07 -0300

    Guess the song project added

--- projects.js
+++ projects.js
@@ -1,54 +1,71 @@
 const projects = [
-    {
-        name: "Tienda Split",
-        img: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F4783cc56-5abe-435e-b45f-b945b77d8c9b%2FsorteoMesa_de_trabajo_1_copiatribus.png?table=block&id=765a6466-e818-4955-ba95-24e197e1cfa0&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
-        repo: "https://github.com/lucasmmaidana/Tienda-Split",
-        demo: "https://tienda-split.vercel.app/",
-        stack: ["HTML","SCSS","JavaScript"],
-        description: "Ecommerce integrated with WhatsApp"
-    },
-    {
-        name: "SIMGRAF-IO",
-        img: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F63d9cc04-0815-42bd-b47c-2570e2087ba4%2FCapture.png?table=block&id=aaa1b445-77eb-41f0-a9a8-cd3524fdee3b&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
-        repo: "https://github.com/lucasmmaidana/SIMGRAF-IO-Simplex/",
-        demo: "https://simplex-calc.netlify.app/",
-        stack: ["HTML","CSS","JavaScript"],
-        description: "Simplex method calculator"
-    },
-    {
-        name: "Tasks Dashboard",
-        img: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fff04e055-3066-41a2-8cc1-41a57983dbad%2Fkids_dashboard.jpg?table=block&id=e7037ce5-d734-4d47-9769-8fcfab8b783a&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
-        repo: "https://github.com/lucasmmaidana/kids-dahsboard",
-        demo: "https://kids-dashboard.netlify.app/",
-        stack: ["HTML","CSS","JavaScript"],
-        description: "App dashboard UI"
-    },
-    {        
-        name: "Skinland E-Commerce",
-        img: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fca52e618-2e05-4967-b3d6-785d7f87731c%2Fskinland.design_.png?table=block&id=68df06ac-32a3-4ffc-ba0d-6ed0de460d41&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
-        repo: null,
-        demo: "https://www.notion.so/lucasmaidana/Skinland-E-Commerce-68df06ac32a34ffcba0d6ed0de460d41",
-        stack: ["WordPress","PHP","CSS"],
-        description: ""
-    },
-    {        
-        name: "New client form",
-        img: "https://www.notion.so/image/https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Ffs%2F17e47680444833.5ce1cbb0e2511.png?table=block&id=5a67561c-9b03-41a5-8af8-7a61e60b6ca7&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
-        repo: "https://codepen.io/LucasMaidana/pen/PazqaV",
-        demo: "https://codepen.io/LucasMaidana/pen/PazqaV",
-        stack: ["HTML","CSS","JavaScript"],
-        description: "UI for a gym's management system. Browser input validation. Microinteractions"
-    },
-    
-    {        
-        name: "Fantasmas",
-        img: "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd7a30fbe-6736-477d-98a7-41143611e5e8%2FCaptura_de_pantalla_2020-08-04_-_12.24.41.png?table=block&id=de334ce9-076b-43ae-a4aa-e8c248caa00b&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
-        repo: null,
-        demo: "https://jumpers-game.blogspot.com/",
-        stack: ["HTML","CSS","JavaScript"],
-        description: "HTML5 game with CSS animations."
-    },
+  {
+    name: "Guess the song",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F45721836-cd86-4f38-9310-a2f4de8f1248%2Fbehance_cover_Taylor_swift_guess_the_song.png?table=block&id=5470237a-0e48-4b53-8d08-c7e1caaaacf4&width=3490&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: "https://github.com/lucasmmaidana/taylor-guess-the-song",
+    demo:
+      "https://www.figma.com/proto/F36sSIDponxiFMix3ALfK2/Guess-the-Song-Taylor-Swift?node-id=1%3A3&scaling=min-zoom",
+    stack: ["React", "SCSS", "Figma"],
+    description: "Project in progress. Quizz game in ReactJS.",
+  },
+  {
+    name: "Tienda Split",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F4783cc56-5abe-435e-b45f-b945b77d8c9b%2FsorteoMesa_de_trabajo_1_copiatribus.png?table=block&id=765a6466-e818-4955-ba95-24e197e1cfa0&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: "https://github.com/lucasmmaidana/Tienda-Split",
+    demo: "https://tienda-split.vercel.app/",
+    stack: ["HTML", "SCSS", "JavaScript"],
+    description: "Ecommerce integrated with WhatsApp",
+  },
+  {
+    name: "SIMGRAF-IO",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F63d9cc04-0815-42bd-b47c-2570e2087ba4%2FCapture.png?table=block&id=aaa1b445-77eb-41f0-a9a8-cd3524fdee3b&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: "https://github.com/lucasmmaidana/SIMGRAF-IO-Simplex/",
+    demo: "https://simplex-calc.netlify.app/",
+    stack: ["HTML", "CSS", "JavaScript"],
+    description: "Simplex method calculator",
+  },
+  {
+    name: "Tasks Dashboard",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fff04e055-3066-41a2-8cc1-41a57983dbad%2Fkids_dashboard.jpg?table=block&id=e7037ce5-d734-4d47-9769-8fcfab8b783a&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: "https://github.com/lucasmmaidana/kids-dahsboard",
+    demo: "https://kids-dashboard.netlify.app/",
+    stack: ["HTML", "CSS", "JavaScript"],
+    description: "App dashboard UI",
+  },
+  {
+    name: "Skinland E-Commerce",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fca52e618-2e05-4967-b3d6-785d7f87731c%2Fskinland.design_.png?table=block&id=68df06ac-32a3-4ffc-ba0d-6ed0de460d41&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: null,
+    demo:
+      "https://www.notion.so/lucasmaidana/Skinland-E-Commerce-68df06ac32a34ffcba0d6ed0de460d41",
+    stack: ["WordPress", "PHP", "CSS"],
+    description: "",
+  },
+  {
+    name: "New client form",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fmir-s3-cdn-cf.behance.net%2Fproject_modules%2Ffs%2F17e47680444833.5ce1cbb0e2511.png?table=block&id=5a67561c-9b03-41a5-8af8-7a61e60b6ca7&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: "https://codepen.io/LucasMaidana/pen/PazqaV",
+    demo: "https://codepen.io/LucasMaidana/pen/PazqaV",
+    stack: ["HTML", "CSS", "JavaScript"],
+    description:
+      "UI for a gym's management system. Browser input validation. Microinteractions",
+  },
 
+  {
+    name: "Fantasmas",
+    img:
+      "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fd7a30fbe-6736-477d-98a7-41143611e5e8%2FCaptura_de_pantalla_2020-08-04_-_12.24.41.png?table=block&id=de334ce9-076b-43ae-a4aa-e8c248caa00b&width=2790&userId=4bb9f704-7013-46e4-a417-bae0aa696d45&cache=v2",
+    repo: null,
+    demo: "https://jumpers-game.blogspot.com/",
+    stack: ["HTML", "CSS", "JavaScript"],
+    description: "HTML5 game with CSS animations.",
+  },
 ]
 
 export default projects
