802b269e71fb42e5142c3fe1fa329b8610db926b
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-01-16 18:02:53 -0300

    Line breaks in descriptions

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
-import projects from './projects.js'
+import projects from "./projects.js"
 
-const projectsDiv = document.getElementById('projects-list')
+const projectsDiv = document.getElementById("projects-list")
 
 let html = ""
 
-projects.forEach(project => {
+projects.forEach((project) => {
+  let stack = ""
+  project.stack.forEach((tech) => {
+    stack += `<span>${tech}</span> `
+  })
 
-    let stack = ""
-    project.stack.forEach(tech => {
-        stack += `<span>${tech}</span> `
-    })
-
-    html += `
+  html += `
         <div class="project">
             <div class="project__img">
-             <a href="${project.demo && project.demo}"><img src="${project.img}" alt="${project.name}"></a>
+             <a href="${project.demo && project.demo}"><img src="${
+    project.img
+  }" alt="${project.name}"></a>
             </div>
             <div class="project__info">
                 <h3>${project.name}</h3>
                 <div class="project__stack">
                 ${stack}
                 </div>
-                <p class="project__description">${project.description}</p>
+                <p class="project__description">${project.description.replace(
+                  /(?:\r\n|\r|\n)/g,
+                  "<br>"
+                )}</p>
                 <div class="project__links">
-                    ${project.demo ? `<a href="${project.demo}">Demo</a>` : "" }
-                    ${project.repo ? `<a href="${project.repo}">Code</a>` : "" }
+                    ${project.demo ? `<a href="${project.demo}">Demo</a>` : ""}
+                    ${project.repo ? `<a href="${project.repo}">Code</a>` : ""}
                 </div>
             </div>
         </div>
